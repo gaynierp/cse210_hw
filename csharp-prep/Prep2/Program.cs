54bbd1a3f6c4f1ce2f760113fa80df3360d74898
gaynierp <gay22004@byui.edu>
2026-09-22 17:33:34 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter = "";
+        Console.WriteLine("What grade did you get? Just type the number like 99");
+        grade = int.Parse(Console.ReadLine());
+        if (grade > 90)
+        {
+            letter = "A";
+        }
+        else if (grade > 80)
+        {
+            letter = "B";
+        }
+        else if (grade > 70)
+        {
+            letter = "C";
+        }
+        else if (grade > 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+
+        if (letter == "C" || letter == "B" || letter == "A")
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass but try again!");
+        }
+        }
     }
-}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Another change");
+        int x = 12;
+        int y = 22;
+        int z = 4;
+        if (x == 10) {
+            Console.WriteLine("X is 10");
+            Console.WriteLine ("Y is fun");
+        }
+        else if (x == 5 || z == 4 && z ==5)
+        {
+            Console.WriteLine("Z = 4");
+        }
+        else if (x > 4 || y != 20)
+        {
+            Console.WriteLine("Print");
+        }
+        else {
+            Console.WriteLine("Default output");
+        }
     }
 }
