e645b62da52a42856c467b92bda04c1641f40938
gaynierp <gay22004@byui.edu>
2026-09-22 09:11:44 -0600

    Update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.Write("Please enter your name: ");
+        firstName = Console.ReadLine();
+        Console.WriteLine("Please enter you last name");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"{firstName} {lastName}");
     }
 }
