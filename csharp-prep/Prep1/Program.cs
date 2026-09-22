a75500f7d4a1222466aa372e579778d9849c88f5
gaynierp <gay22004@byui.edu>
2026-09-22 09:21:39 -0600

    Finish prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        // C# is strongly typed. Set variables before you can use it
+        // also you can't change data type
         string firstName;
         string lastName;
+        // .Write enters input on the same line
         Console.Write("Please enter your name: ");
         firstName = Console.ReadLine();
-        Console.WriteLine("Please enter you last name");
+        // .WriteLine enters input on a new line
+        Console.WriteLine("Please enter your last name: ");
         lastName = Console.ReadLine();
-        Console.WriteLine($"{firstName} {lastName}");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
